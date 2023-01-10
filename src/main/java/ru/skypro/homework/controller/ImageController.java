5a8ef150d98ce6858e543cfd7a7112cff21d97ae
VyacheslavChik22 <asyofka@mail.ru>
2023-01-10 22:55:01 +0300

    Создал ветку Slava, ImageController

---
+++ src/main/java/ru/skypro/homework/controller/ImageController.java
@@ -0,0 +1,27 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.service.ImageService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping
+public class ImageController {
+
+    private  final ImageService imageService;
+
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PatchMapping (value = "/image/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> updateAdsImage(@PathVariable Long id, @RequestParam MultipartFile image){
+      String[] images = {"image_001"};
+        return ResponseEntity.ok().build();
+    }
+
+}
