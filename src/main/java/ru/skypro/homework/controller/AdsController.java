9cd0ca53589c8df4691f4765f50651def263da35
Lexxkit <lexxkit@gmail.com>
2023-01-10 12:18:04 +0000

    Created Ads & ResponseWrapperAds classes in dto package; created AdsController and getAds method; commented line in WebSecurityConfig;

---
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -0,0 +1,21 @@
+package ru.skypro.homework.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.homework.dto.ResponseWrapperAds;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping(path = "/ads")
+public class AdsController {
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapperAds> getAds() {
+        return ResponseEntity.ok(new ResponseWrapperAds());
+    }
+}
