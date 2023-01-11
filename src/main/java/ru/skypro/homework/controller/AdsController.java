af335a73e99af4d8f78c6a4f849f69eece7de1ad
Lexxkit <lexxkit@gmail.com>
2023-01-11 17:41:16 +0000

    add swagger description to 5 methods in AdsController;

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -1,5 +1,9 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +24,19 @@ public class AdsController {
         return ResponseEntity.ok(new ResponseWrapperAds());
     }
 
+    @Operation(summary = "addAds",
+            responses = {
+            @ApiResponse(
+                    responseCode = "201",
+                    content = @Content(
+                            mediaType = MediaType.ALL_VALUE,
+                            schema = @Schema(implementation = Ads.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "401", content = @Content),
+            @ApiResponse(responseCode = "403", content = @Content),
+            @ApiResponse(responseCode = "404", content = @Content)
+    })
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Ads> addAds(@RequestPart(value = "properties") CreateAds createAds,
                                       @RequestPart(value = "image") MultipartFile image) {
@@ -27,28 +44,69 @@ public class AdsController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new Ads());
     }
 
+    @Operation(summary = "getComments",
+            responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            mediaType = MediaType.ALL_VALUE,
+                            schema = @Schema(implementation = ResponseWrapperComment.class)
+                    )
+            ),
+            @ApiResponse(responseCode = "404", content = @Content)
+    })
     @GetMapping("/{ad_pk}/comments")
     public ResponseEntity<ResponseWrapperComment> getComments(@PathVariable(name = "ad_pk") String adPk) {
         log.info("Was invoked get all comments for ad = {} method", adPk);
         return ResponseEntity.ok(new ResponseWrapperComment());
     }
 
+    @Operation(summary = "addComments",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = MediaType.ALL_VALUE,
+                                    schema = @Schema(implementation = Comment.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", content = @Content),
+                    @ApiResponse(responseCode = "403", content = @Content),
+                    @ApiResponse(responseCode = "404", content = @Content)
+            })
     @PostMapping("/{ad_pk}/comments")
     public ResponseEntity<Comment> addComments(@PathVariable(name = "ad_pk") String adPk, @RequestBody Comment comment) {
         log.info("Was invoked add comment for ad = {} method", adPk);
         return ResponseEntity.ok(new Comment());
     }
 
+    @Operation(summary = "getFullAd",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = MediaType.ALL_VALUE,
+                                    schema = @Schema(implementation = FullAds.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "404", content = @Content)
+            })
     @GetMapping("/{id}")
     public ResponseEntity<FullAds> getFullAd(@PathVariable int id) {
         log.info("Was invoked get full ad by id = {} method", id);
         return ResponseEntity.ok(new FullAds());
     }
 
+    @Operation(summary = "removeAds",
+            responses = {
+                    @ApiResponse(responseCode = "204", content = @Content),
+                    @ApiResponse(responseCode = "401", content = @Content),
+                    @ApiResponse(responseCode = "403", content = @Content)
+            })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> removeAds(@PathVariable int id) {
         log.info("Was invoked delete ad by id = {} method", id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{id}")
