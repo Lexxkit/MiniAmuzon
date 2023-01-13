7859766fb451ebbe826e763db71e520e3fdb9bcd
Lexxkit <lexxkit@gmail.com>
2023-01-13 08:57:58 +0000

    add integration test for created controllers in HomeworkApplicationTests;

--- src/test/java/ru/skypro/homework/HomeworkApplicationTests.java
+++ src/test/java/ru/skypro/homework/HomeworkApplicationTests.java
@@ -1,13 +1,33 @@
 package ru.skypro.homework;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import ru.skypro.homework.controller.AdsController;
+import ru.skypro.homework.controller.ImageController;
+import ru.skypro.homework.controller.UserController;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HomeworkApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private AdsController adsController;
+    @Autowired
+    private UserController userController;
+    @Autowired
+    private ImageController imageController;
 
     @Test
     void contextLoads() {
+        assertThat(adsController).isNotNull();
+        assertThat(userController).isNotNull();
+        assertThat(imageController).isNotNull();
     }
 
 }
