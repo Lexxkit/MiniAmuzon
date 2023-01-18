a6ead25e89d674bc5d0ff923c32cfb03c5682446
Lexxkit <lexxkit@gmail.com>
2023-01-18 13:25:40 +0000

    minor changes;

--- src/main/java/ru/skypro/homework/mapper/AdsMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdsMapper.java
@@ -3,13 +3,14 @@ package ru.skypro.homework.mapper;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import ru.skypro.homework.dto.AdsDto;
 import ru.skypro.homework.dto.ResponseWrapperAds;
 import ru.skypro.homework.entity.Ads;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AdsMapper {
     @Mapping(source = "author.id", target = "author")
     @Mapping(source = "id", target = "pk")
