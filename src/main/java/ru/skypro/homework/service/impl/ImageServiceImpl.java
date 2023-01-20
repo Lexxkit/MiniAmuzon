73c4f6a19cd2d8d24a431b1fa0b6707859fe9059
Lexxkit <lexxkit@gmail.com>
2023-01-20 09:02:02 +0000

    minor changes after merging;

--- src/main/java/ru/skypro/homework/service/impl/ImageServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/ImageServiceImpl.java
@@ -2,7 +2,6 @@ package ru.skypro.homework.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-<<<<<<< HEAD
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.entity.Image;
@@ -12,22 +11,12 @@ import ru.skypro.homework.service.ImageService;
 
 import java.io.IOException;
 
-=======
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-import ru.skypro.homework.entity.Ads;
-import ru.skypro.homework.service.AdsService;
-import ru.skypro.homework.service.ImageService;
-
->>>>>>> feature/Slava
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class ImageServiceImpl implements ImageService {
-<<<<<<< HEAD
     private final ImageRepository imageRepository;
+
     @Override
     public byte[] updateAdsImage(long id, MultipartFile file) throws IOException {
         log.info("Was invoked findAllAds method from {}", ImageService.class.getSimpleName());
@@ -42,13 +31,5 @@ public class ImageServiceImpl implements ImageService {
             imageRepository.save(oldImage);
         }
         return imageData;
-=======
-    private  final AdsService adsService;
-
-    @Override
-    public byte[] updateAdsImage(Long id, MultipartFile image) {
-          Ads ads = adsService.getAdsById(id);
-         return null;
->>>>>>> feature/Slava
     }
 }
